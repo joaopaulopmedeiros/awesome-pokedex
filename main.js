779947286f46029e9bb6a1f062b5636bd31a3428
joao paulo <joaopaulopmedeiros@gmail.com>
2020-05-26 14:15:14 -0300

    logging html structure

--- main.js
+++ main.js
@@ -3,11 +3,24 @@ const fetchPokemon = () => {
     
     const pokemonPromises = []
 
-    for(let i = 1; i <= 150; i++){
+    for(let i = 1; i <= 10; i++){
         pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
     }
     Promise.all(pokemonPromises).then(pokemons => {
-        console.log(pokemons)
+        const listOfPokemons = pokemons.reduce((accumulator, pokemon) => {
+            
+            const types = pokemon.types.map(typeInfo => typeInfo.type.name).join('|')
+
+            return accumulator += 
+                `<li class="card">
+                    <img class="card-image ${types[0]} alt=${pokemon.name} src=https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
+                    <h2 class="card-title> ${pokemon.id}. ${pokemon.name}</h2>
+                    <p class="card-subtitle"> ${types} </p>
+                </li>`        
+            }, '')
+
+        console.log(listOfPokemons)
+        
     })
 }
 
