c687ff873892c392d4e460b76fd1c43d38c8481a
joao paulo <joaopaulopmedeiros@gmail.com>
2020-05-26 14:31:54 -0300

    functional approach

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-const fetchPokemon = () => {
-    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
-    
-    const pokemonPromises = []
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
-    for(let i = 1; i <= 8; i++){
-        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
-    }
+const fetchPokemon = () => {
+    
+    const generatePokemonPromises = () => Array(8).fill().map((_,index) => fetch(getPokemonUrl(index+1)).then(response => response.json()))
+
+    const pokemonPromises = generatePokemonPromises()
+    
     Promise.all(pokemonPromises).then(pokemons => {
         const listOfPokemons = pokemons.reduce((accumulator, pokemon) => {
             
             const types = pokemon.types.map(typeInfo => typeInfo.type.name).join('|')
 
             return accumulator += 
-                `<li class="card">
-                    <img class="card-image ${types[0]} alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
+                `<li class="card ${types[0]}">
+                    <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
                     <h2 class="card-title"> ${pokemon.id}. ${pokemon.name}</h2>
                     <p class="card-subtitle"> ${types} </p>
                 </li>`        
