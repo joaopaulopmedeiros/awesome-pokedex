e940ccefe567ae608687164cc120754c133eda94
joao paulo <joaopaulopmedeiros@gmail.com>
2020-05-26 14:02:54 -0300

    dynamic url

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const fetchPokemon = () => {
-    const url = 'https://pokeapi.co/api/v2/pokemon'
-    fetch(url)
-        .then(response => response.json())
-        .then(pokemon => console.log(pokemon))
+    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+    
+    const pokemonPromises = []
+
+    for(let i = 1; i <= 150; i++){
+        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
+    }
+    Promise.all(pokemonPromises).then(pokemons => {
+        console.log(pokemons)
+    })
 }
 
 fetchPokemon()
